ce19aa2359ec5fbf2fafe5c2b507c7e50a3384b6
rickypang0219 <rickypang0219@gmail.com>
2024-06-11 01:38:22 +0800

    write bb band strat in RUST

---
+++ bollinger_band.py
@@ -0,0 +1,141 @@
+import pandas as pd
+import numpy as np
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Read the PI Excel file
+data = pd.read_csv("PI_1H.csv")
+data = data.set_index("Time")
+pi_data = data.dropna()
+
+
+# Read the BTC Excel file
+data = pd.read_csv("BTC_price_1H.csv")
+data = data.set_index("Time")
+btc_data = data.dropna()
+
+
+# Vectorization
+pi_data_High = pi_data["High"].to_numpy()
+pi_data_Low = pi_data["Low"].to_numpy()
+BTC_High = btc_data["High"].to_numpy()
+BTC_Low = btc_data["Low"].to_numpy()
+BTC_Open = btc_data["Open"].to_numpy()
+BTC_Close = btc_data["Close"].to_numpy()
+
+
+def pi_1h_strategy(btc_data, pi_data, period, bb_multiplier):
+    # Vectorization
+    pi_high_upper = (
+        pi_data["High"].rolling(window=period).mean()
+        + bb_multiplier * pi_data["High"].rolling(window=period).std()
+    )
+    pi_low_lower = (
+        pi_data["Low"].rolling(window=period).mean()
+        - bb_multiplier * pi_data["Low"].rolling(window=period).std()
+    )
+
+    pi_high_upper = pi_high_upper.to_numpy()
+    pi_low_lower = pi_low_lower.to_numpy()
+
+    # Initialize signal, position, and P&L columns
+    BTC_Entry_Signal = np.zeros((len(btc_data)))
+    BTC_Signal_with_exits = np.zeros((len(btc_data)))
+    BTC_Position = np.zeros((len(btc_data)))
+    BTC_Pnl = np.zeros((len(btc_data)))
+    BTC_mid_line = np.zeros((len(btc_data)))
+
+    BTC_mid_line = (pi_high_upper + pi_low_lower) / 2
+
+    for i in range(2, len(pi_data)):
+        if (
+            pi_data_High[i - 2] < pi_high_upper[i - 2]
+            and pi_data_High[i - 1] > pi_high_upper[i - 1]
+            and BTC_Position[i - 1] == 0
+        ):
+            BTC_Entry_Signal[i] = -1
+            BTC_Position[i] = BTC_Position[i - 1] - 1
+        elif (
+            pi_data_Low[i - 2]
+            > pi_low_lower[i - 2]  # Pi low crossbelow the pi_low lower band
+            and pi_data_Low[i - 1] < pi_low_lower[i - 1]
+            and BTC_Position[i - 1] == 0  # No position
+        ):
+            BTC_Entry_Signal[i] = 1  # Long signal , Open long position
+            BTC_Position[i] = BTC_Position[i - 1] + 1
+
+        else:
+            BTC_Entry_Signal[i] = 0  # No signal
+            BTC_Position[i] = BTC_Position[i - 1]
+
+        # Exit conditions
+        if (
+            BTC_Position[i - 1] == 1
+            and pi_data_Low[i - 2] < BTC_mid_line[i - 2]
+            and pi_data_Low[i - 1] > BTC_mid_line[i - 1]
+        ):
+            BTC_Position[i] = 0  # Flat Long signal
+
+        elif (
+            BTC_Position[i - 1] == -1
+            and pi_data_High[i - 2] > BTC_mid_line[i - 2]
+            and pi_data_High[i - 1] < BTC_mid_line[i - 1]
+        ):
+            BTC_Position[i] = 0  # Flat Short signal
+
+        # # Signal with exits
+        # if BTC_Entry_Signal[i] == -1:
+        #     BTC_Signal_with_exits[i] = BTC_Entry_Signal[i]
+        # elif BTC_Entry_Signal[i] == 1:
+        #     BTC_Signal_with_exits[i] = BTC_Entry_Signal[i]
+        # elif (
+        #     BTC_Entry_Signal[i] == 0
+        #     and BTC_Position[i - 1] == -1
+        #     and BTC_Position[i] == 0  # Flat short pos
+        # ):
+        #     BTC_Signal_with_exits[i] = 1
+        # elif (
+        #     BTC_Entry_Signal[i] == 0
+        #     and BTC_Position[i - 1] == 1
+        #     and BTC_Position[i] == 0  # Flat long pos
+        # ):
+        #     BTC_Signal_with_exits[i] = -1
+        # else:
+        #     BTC_Signal_with_exits[i] = 0
+
+        # Calculate P&L
+        if BTC_Position[i] == -1:
+            entry_price = BTC_Open[i]
+            exit_price = BTC_Close[i]
+            BTC_Pnl[i] = -((exit_price / entry_price) - 1)
+        elif BTC_Position[i] == 1:
+            entry_price = BTC_Open[i]
+            exit_price = BTC_Close[i]
+            BTC_Pnl[i] = (exit_price / entry_price) - 1
+
+        # Transaction cost
+        if BTC_Signal_with_exits[i] == 1 or BTC_Signal_with_exits[i] == -1:
+            BTC_Pnl[i] = BTC_Pnl[i] - (0.06 / 100)
+
+    # Calculate cumulative P&L
+    BTC_CUM_PNL = np.cumsum(BTC_Pnl)
+
+    return (
+        BTC_Open,
+        BTC_High,
+        BTC_Low,
+        BTC_Close,
+        BTC_Entry_Signal,
+        BTC_Signal_with_exits,
+        BTC_Position,
+        BTC_Pnl,
+        BTC_mid_line,
+        BTC_CUM_PNL,
+    )
+
+
+if __name__ == "__main__":
+    res = pi_1h_strategy(btc_data, pi_data, 100, 2)
+    BTC_CUM_PNL = res[9]
+    plt.plot(BTC_CUM_PNL)
+    plt.show()
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use csv::Reader;
+use plotly::{Plot, Scatter};
+use statistics::mean;
+use statistics::variance;
 use std::error::Error;
 
 fn read_csv(file_path: &str) -> Result<(Vec<f64>, Vec<f64>, Vec<f64>, Vec<f64>), Box<dyn Error>> {
@@ -23,15 +26,212 @@ fn read_csv(file_path: &str) -> Result<(Vec<f64>, Vec<f64>, Vec<f64>, Vec<f64>),
     Ok((open_vec, high_vec, low_vec, close_vec))
 }
 
+fn strategy(
+    pi_open: Vec<f64>,
+    pi_high: Vec<f64>,
+    pi_low: Vec<f64>,
+    _pi_close: Vec<f64>,
+    btc_open: Vec<f64>,
+    _btc_high: Vec<f64>,
+    _btc_low: Vec<f64>,
+    btc_close: Vec<f64>,
+    window_size: usize,
+    bb_multiplier: f64,
+) -> Vec<f64> {
+    let pi_higher: Vec<f64> = calculate_rolling_mean(&pi_high, window_size)
+        .iter()
+        .zip(calculate_rolling_std(&pi_high, window_size).iter())
+        .map(|(&mean, &std)| mean + bb_multiplier * std)
+        .collect();
+
+    let pi_lower: Vec<f64> = calculate_rolling_mean(&pi_low, window_size)
+        .iter()
+        .zip(calculate_rolling_std(&pi_low, window_size).iter())
+        .map(|(&mean, &std)| mean - bb_multiplier * std)
+        .collect();
+
+    let mut btc_entry_signal: Vec<i64> = vec![0; pi_open.len()];
+    let mut btc_signal_with_exits: Vec<i64> = vec![0; pi_open.len()];
+    let mut btc_position: Vec<i64> = vec![0; pi_open.len()];
+    let mut btc_pnl: Vec<f64> = vec![0.0; pi_open.len()];
+    let btc_mid_line: Vec<f64> = pi_higher
+        .iter()
+        .zip(pi_lower.iter())
+        .map(|(&a, &b)| (a + b) / 2.0)
+        .collect();
+
+    println!("{}", (pi_higher.len()));
+    for i in 2..(pi_open.len() - 1) {
+        if (pi_high[i - 2] < pi_higher[i - 2])
+            && (pi_high[i - 1] > pi_higher[i - 1])
+            && (btc_position[i - 1] == 0)
+        {
+            btc_entry_signal[i] = -1;
+            btc_position[i] = btc_position[i - 1] - 1;
+        } else if (pi_low[i - 2] > pi_lower[i - 2])
+            && (pi_low[i - 1] < pi_lower[i - 1])
+            && (btc_position[i - 1] == 0)
+        {
+            btc_entry_signal[i] = 1;
+            btc_position[i] = btc_position[i - 1] + 1;
+        } else {
+            btc_entry_signal[i] = 0;
+            btc_position[i] = btc_position[i - 1];
+        };
+        // Exit condition
+        if (btc_position[i - 1] == 1)
+            && (pi_low[i - 2] < btc_mid_line[i - 2])
+            && (pi_low[i - 1] > btc_mid_line[i - 1])
+        {
+            btc_position[i] = 0;
+        } else if (btc_position[i - 1] == -1)
+            && (pi_high[i - 2] > btc_mid_line[i - 2])
+            && (pi_high[i - 1] < btc_mid_line[i - 1])
+        {
+            btc_position[i] = 0;
+        };
+        // Signal with exit
+        if (btc_entry_signal[i] == -1) {
+            btc_signal_with_exits[i] = btc_entry_signal[i];
+        } else if (btc_entry_signal[i] == 1) {
+            btc_signal_with_exits[i] = btc_entry_signal[i];
+        } else if (btc_entry_signal[i] == 0)
+            && (btc_position[i - 1] == -1)
+            && (btc_position[i] == 0)
+        {
+            btc_signal_with_exits[i] = 1;
+        } else if (btc_entry_signal[i] == 0) && (btc_position[i - 1] == 1) && (btc_position[i] == 0)
+        {
+            btc_signal_with_exits[i] = -1;
+        } else {
+            btc_signal_with_exits[i] = 0;
+        };
+        // Calculate PnL
+        if btc_position[i] == -1 {
+            let entry_price: f64 = btc_open[i];
+            let exit_price: f64 = btc_close[i];
+            btc_pnl[i] = -((exit_price / entry_price) - 1.0);
+        } else if btc_position[i] == 1 {
+            let entry_price: f64 = btc_open[i];
+            let exit_price: f64 = btc_close[i];
+            btc_pnl[i] = (exit_price / entry_price) - 1.0;
+        };
+        // Transaction cost
+        if (btc_signal_with_exits[i] == 1) | (btc_signal_with_exits[i] == -1) {
+            btc_pnl[i] = btc_pnl[i] - 0.06 / 100.0;
+        }
+    }
+    btc_pnl
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let result = read_csv("PI_1H.csv")?;
-    let open_prices: Vec<f64> = result.0;
-    let high_prices: Vec<f64> = result.1;
-    let low_prices: Vec<f64> = result.2;
-    let close_prices: Vec<f64> = result.3;
+    let pi_open: Vec<f64> = result.0;
+    let pi_high: Vec<f64> = result.1;
+    let pi_low: Vec<f64> = result.2;
+    let pi_close: Vec<f64> = result.3;
 
-    for price in open_prices {
-        println!("{}", price);
+    let btc_data = read_csv("BTC_price_1H.csv")?;
+    let btc_open: Vec<f64> = btc_data.0;
+    let btc_high: Vec<f64> = btc_data.1;
+    let btc_low: Vec<f64> = btc_data.2;
+    let btc_close: Vec<f64> = btc_data.3;
+
+    let mut count: Vec<usize> = Vec::new();
+    for i in 0..(pi_high.len()) {
+        count.push(i);
     }
+
+    let window_size: usize = 100;
+    let bb_multiplier: f64 = 2.0;
+
+    let btc_pnl: Vec<f64> = strategy(
+        pi_open,
+        pi_high,
+        pi_low,
+        pi_close,
+        btc_open,
+        btc_high,
+        btc_low,
+        btc_close,
+        window_size,
+        bb_multiplier,
+    );
+    // println!("{:?}", btc_pnl);
+    let btc_pnl_cum_sum: Vec<f64> = calculate_cumulative_sum(&btc_pnl);
+    let mut plot = Plot::new();
+    let trace = Scatter::new(count, btc_pnl_cum_sum);
+    plot.add_trace(trace);
+    plot.show();
+
     Ok(())
+}
+
+fn calculate_rolling_mean(data: &[f64], window_size: usize) -> Vec<f64> {
+    let mut rolling_mean: Vec<f64> = data
+        .windows(window_size)
+        .map(|window| mean(window))
+        .collect();
+    for i in 0..(window_size - 1) {
+        rolling_mean.insert(i, std::f64::NAN);
+    }
+    rolling_mean
+}
+
+fn calculate_rolling_std(data: &[f64], window_size: usize) -> Vec<f64> {
+    let mut rolling_std: Vec<f64> = data
+        .windows(window_size)
+        .map(|window| variance(window).sqrt())
+        .collect();
+    for i in 0..(window_size - 1) {
+        rolling_std.insert(i, std::f64::NAN);
+    }
+    rolling_std
+}
+
+fn calculate_cumulative_sum(numbers: &[f64]) -> Vec<f64> {
+    numbers
+        .iter()
+        .scan(0.0, |acc, &x| {
+            *acc += x;
+            Some(*acc)
+        })
+        .collect()
+}
+
+fn strategy_test(
+    _pi_open: Vec<f64>,
+    pi_high: Vec<f64>,
+    pi_low: Vec<f64>,
+    _pi_close: Vec<f64>,
+    _btc_open: Vec<f64>,
+    _btc_high: Vec<f64>,
+    _btc_low: Vec<f64>,
+    _btc_close: Vec<f64>,
+    window_size: usize,
+    bb_multiplier: f64,
+) -> Vec<f64> {
+    let pi_higher: Vec<f64> = calculate_rolling_mean(&pi_high, window_size)
+        .iter()
+        .zip(calculate_rolling_std(&pi_high, window_size).iter())
+        .map(|(&mean, &std)| mean + bb_multiplier * std)
+        .collect();
+
+    let pi_lower: Vec<f64> = calculate_rolling_mean(&pi_low, window_size)
+        .iter()
+        .zip(calculate_rolling_std(&pi_low, window_size).iter())
+        .map(|(&mean, &std)| mean + bb_multiplier * std)
+        .collect();
+
+    let btc_mid_line: Vec<f64> = pi_higher
+        .iter()
+        .zip(pi_lower.iter())
+        .map(|(&a, &b)| (a + b) / 2.0)
+        .collect();
+
+    let roll_mean: Vec<f64> = calculate_rolling_mean(&pi_high, 100);
+
+    println!("{}", pi_higher.len());
+    println!("{}", roll_mean.len());
+    btc_mid_line
 }
